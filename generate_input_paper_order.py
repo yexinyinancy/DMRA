375d4030e4271ff21305a63239745f41a0a2ba6a
yexinyinancy <zj20140116@163.com>
2021-12-12 16:58:13 -0500

    update generate data code

--- generate_input_paper_order.py
+++ generate_input_paper_order.py
@@ -1,20 +1,25 @@
 import random
 import tqdm
 
-SERVICE_NUM = 30
+SERVICE_NUM = 10
 SPNUM = 5
 BSNUM = 25
-PRICE_SP = 10
+PRICE_SP = 20
 GRIDX = 1200
 GRIDY = 1200
-BAN = 100000
-RES_BS = 30
+BAN_MIN = 800
+BAN_MAX = 1000
+RES_BS_MIN = 100
+RES_BS_MAX = 150
 PRICE_BS = 10
 COMPUTATIONPOWER = 10
-UENUM = [400,500,600,700,800,900]
-RES_UE = 10
-RES_RADIO = 0
-WORKLOAD = 10
+UENUM = [100,200,300,400]
+# UENUM = [400, 500, 600, 700, 800, 900, 1000, 1500, 2000, 3000, 4000, 5000, 10000, 20000, 50000, 100000, 200000, 300000, 500000]
+RES_UE_MIN = 3
+RES_UE_MAX = 5
+RES_RADIO_MIN = 30
+RES_RADIO_MAX = 60
+WORKLOAD = 100
 
 
 
@@ -38,40 +43,50 @@ def createRandomList(num, start, end):
     return arr
 spid_bs = createRandomList(BSNUM, 0, BSNUM-1) # sp belongs to bs
 
+def createRandomListDup(num, start, end):
+    arr = []
+    tmp = random.randint(start, end)
+    for x in range(num):  
+        tmp = random.randint(start, end)  
+        arr.append(tmp)
+    arr.sort() 
+    return arr
+
 bs_position = []
 for i in range(5):
     for j in range(5):
         bs_position.append((i*300, j*300))
 random.shuffle(bs_position)       
 
-random.seed(10)
-for i in range(len(UENUM)):
-    with open('input_paper_order' + str(i) + '.txt', 'w') as f:
-        f.write(str(SERVICE_NUM) + '\n')  
-        f.write('\n')
-        
-        f.write(str(SPNUM) + '\n')
-        for j in range(SPNUM):
-            tmp = str(j)
-            price_sp = random.randint(1,PRICE_SP)
-            for _ in range(SERVICE_NUM):
-                tmp += ' '
-                tmp += str(price_sp)
-            f.write(tmp + '\n')
-        f.write('\n')
-        
-        f.write(str(BSNUM) + '\n') 
-        spid = 0
-        for j in range(BSNUM):
-            f.write(str(bs_position[j][0]) + ' ' + str(bs_position[j][1]) + ' ' + str(BAN) + ' ' + str(6) + ' ' + str(spid_bs.index(j)//5) + '\n')
-            sp_id = createRandomSortedList(6, 0, SERVICE_NUM-1)
-            for k in range(6):
-                f.write(str(sp_id[k]) + ' ' + str(random.randint(1,RES_BS)) + ' ' + str(random.randint(1,PRICE_BS)) + ' ' + str(random.randint(1,COMPUTATIONPOWER)) + '\n')
-        f.write('\n')    
 
-        f.write(str(UENUM[i]) + '\n')   
-        x = createRandomSortedList(UENUM[i], 0, GRIDX-1)
-        for j in range(UENUM[i]):
-            f.write(str(x[j]) + ' ' + str(random.randint(0,GRIDY-1)) + ' ' + str(random.randint(0,SPNUM-1)) + ' ' + str(random.randint(0,SERVICE_NUM-1)) + ' ' + str(random.randint(1,WORKLOAD)) + ' ' + str(random.randint(1,RES_UE))+ ' ' + str(RES_RADIO) + '\n')
-        f.write('\n')
+for i in range(len(UENUM)):
+    f = open('./data4/input_paper_order_ue' + str(UENUM[i]) + '.txt', 'w')
+    f.write(str(SERVICE_NUM) + '\n')  
+    f.write('\n')
+    
+    f.write(str(SPNUM) + '\n')
+    for j in range(SPNUM):
+        tmp = str(j)
+        for _ in range(SERVICE_NUM):
+            tmp += ' '
+            tmp += str(PRICE_SP)
+        f.write(tmp + '\n')
+    f.write('\n')
+    
+    f.write(str(BSNUM) + '\n') 
+    spid = 0
+    random.seed(20)
+    for j in range(BSNUM):
+        f.write(str(bs_position[j][0]) + ' ' + str(bs_position[j][1]) + ' ' + str(random.randint(BAN_MIN,BAN_MAX)) + ' ' + str(6) + ' ' + str(spid_bs.index(j)//5) + '\n')
+        sp_id = createRandomSortedList(6, 0, SERVICE_NUM-1)
+        for k in range(6):
+            f.write(str(sp_id[k]) + ' ' + str(random.randint(RES_BS_MIN,RES_BS_MAX)) + ' ' + str(random.randint(1,PRICE_BS)) + ' ' + str(random.randint(1,COMPUTATIONPOWER)) + '\n')
+    f.write('\n')    
+
+    f.write(str(UENUM[i]) + '\n')   
+    # x = createRandomSortedList(UENUM[i], 0, GRIDX-1)
+    x = createRandomListDup(UENUM[i], 0, GRIDX-1)
+    for j in range(UENUM[i]):
+        f.write(str(x[j]) + ' ' + str(random.randint(0,GRIDY-1)) + ' ' + str(random.randint(0,SPNUM-1)) + ' ' + str(random.randint(0,SERVICE_NUM-1)) + ' ' + str(random.randint(1,WORKLOAD)) + ' ' + str(random.randint(RES_UE_MIN,RES_UE_MAX))+ ' ' + str(random.randint(RES_RADIO_MIN,RES_RADIO_MAX)) + '\n')
+    f.write('\n')
         
--- generate_input_paper_random.py
+++ generate_input_paper_random.py
@@ -1,20 +1,23 @@
 import random
 import tqdm
 
-SERVICE_NUM = 30
+SERVICE_NUM = 10
 SPNUM = 5
 BSNUM = 25
-PRICE_SP = 10
+PRICE_SP = 20
 GRIDX = 1200
 GRIDY = 1200
 BAN = 100000
-RES_BS = 30
+RES_BS_MIN = 100
+RES_BS_MAX = 150
 PRICE_BS = 10
 COMPUTATIONPOWER = 10
-UENUM = [400,500,600,700,800,900]
-RES_UE = 10
+# UENUM = [400,500,600,700,800,900]
+UENUM = [400, 500, 600, 700, 800, 900, 1000, 1500, 2000, 3000, 4000, 5000, 10000, 20000, 50000, 100000, 200000, 300000, 500000]
+RES_UE_MIN = 3
+RES_UE_MAX = 5
 RES_RADIO = 0
-WORKLOAD = 10
+WORKLOAD = 100
 
 
 
@@ -36,36 +39,47 @@ def createRandomList(num, start, end):
             tmp = random.randint(start, end)  
         arr.append(tmp)
     return arr
+
+def createRandomListDup(num, start, end):
+    arr = []
+    tmp = random.randint(start, end)
+    for x in range(num):  
+        tmp = random.randint(start, end)  
+        arr.append(tmp)
+    arr.sort() 
+    return arr
+
 spid_bs = createRandomList(BSNUM, 0, BSNUM-1)
 
-random.seed(10)
+
 for i in range(len(UENUM)):
-    with open('input_paper_random' + str(i) + '.txt', 'w') as f:
+    with open('./data2/input_paper_random_ue' + str(UENUM[i]) + '.txt', 'w') as f:
         f.write(str(SERVICE_NUM) + '\n')  
         f.write('\n')
         
         f.write(str(SPNUM) + '\n')
         for j in range(SPNUM):
             tmp = str(j)
-            price_sp = random.randint(1,PRICE_SP)
             for _ in range(SERVICE_NUM):
                 tmp += ' '
-                tmp += str(price_sp)
+                tmp += str(PRICE_SP)
             f.write(tmp + '\n')
         f.write('\n')
         
         f.write(str(BSNUM) + '\n') 
         spid = 0
+        random.seed(20)
         for j in range(BSNUM):
             f.write(str(random.randint(0,GRIDX-1)) + ' ' + str(random.randint(0,GRIDY-1)) + ' ' + str(BAN) + ' ' + str(6) + ' ' + str(spid_bs.index(j)//5) + '\n')
             sp_id = createRandomSortedList(6, 0, SERVICE_NUM-1)
             for k in range(6):
-                f.write(str(sp_id[k]) + ' ' + str(random.randint(1,RES_BS)) + ' ' + str(random.randint(1,PRICE_BS)) + ' ' + str(random.randint(1,COMPUTATIONPOWER)) + '\n')
+                f.write(str(sp_id[k]) + ' ' + str(random.randint(RES_BS_MIN,RES_BS_MAX)) + ' ' + str(random.randint(1,PRICE_BS)) + ' ' + str(random.randint(1,COMPUTATIONPOWER)) + '\n')
         f.write('\n')    
 
         f.write(str(UENUM[i]) + '\n')   
-        x = createRandomSortedList(UENUM[i], 0, GRIDX-1)
+        # x = createRandomSortedList(UENUM[i], 0, GRIDX-1)
+        x = createRandomListDup(UENUM[i], 0, GRIDX-1)
         for j in range(UENUM[i]):
-            f.write(str(x[j]) + ' ' + str(random.randint(0,GRIDY-1)) + ' ' + str(random.randint(0,SPNUM-1)) + ' ' + str(random.randint(0,SERVICE_NUM-1)) + ' ' + str(random.randint(1,WORKLOAD)) + ' ' + str(random.randint(1,RES_UE))+ ' ' + str(RES_RADIO) + '\n')
+            f.write(str(x[j]) + ' ' + str(random.randint(0,GRIDY-1)) + ' ' + str(random.randint(0,SPNUM-1)) + ' ' + str(random.randint(0,SERVICE_NUM-1)) + ' ' + str(random.randint(1,WORKLOAD)) + ' ' + str(random.randint(RES_UE_MIN,RES_UE_MAX))+ ' ' + str(RES_RADIO) + '\n')
         f.write('\n')
         
